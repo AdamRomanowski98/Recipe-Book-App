c7d7a4e4711c5816e1042a022526351facd3e806
Adam <adam.romanowski98@gmail.com>
2022-09-05 22:27:30 +0200

    Added RecipeController

--- src/main/java/com/spring/recipebook/services/RecipeService.java
+++ src/main/java/com/spring/recipebook/services/RecipeService.java
@@ -2,9 +2,13 @@ package com.spring.recipebook.services;
 
 import com.spring.recipebook.model.Recipe;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface RecipeService {
 
     Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
+
 }
